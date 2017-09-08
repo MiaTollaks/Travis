fa111240d5ee0b49d2a06e568466d481869b0a23
miatollaksvik <design@miatollaksvik.no>
2017-09-08 20:11:13 +0200

    nav finish scrolling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,62 @@
 $(function(){
+    smoothScroll(300);
     mentoringBubbleClick();  
     setInterval(function(){ articleTada();}, 3000)
     designBGStuff();
     mobileNav();
     
 });
+
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+// Cache selectors
+var topMenu = $("#site-nav"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   // Set/remove active class
+   menuItems
+     .parent().removeClass("active")
+     .end().filter("[href='#"+id+"']").parent().addClass("active");
+});
+
+
+
+
+
 
 // TOGGL MOBILE NAV 
 function mobileNav() {
